06df62b4a1742a168b7a1e8c95f8e8c28c20c50d
Ravi K <ravi4kumawat@gmail.com>
2026-01-30 15:32:58 +0530

    Refactor StringCalculator to support default and custom delimiters

--- culture-and-tdd/string-kata/string_calculator.rb
+++ culture-and-tdd/string-kata/string_calculator.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class StringCalculator
+  DEFAULT_DELIMITER = ','
+  CUSTOM_DELIMITER_PREFIX = '//'
+
   def add(numbers_str)
     number_list = extract_numbers(numbers_str)
     number_list.sum
@@ -11,12 +14,19 @@ class StringCalculator
   def extract_numbers(numbers_str)
     return [] if numbers_str.empty?
 
-    delimiter = ','
-    if numbers_str.start_with?('//')
-      delimiter = numbers_str.split('//')[1][0]
+    delimiter, numbers = split_delimiter_and_numbers(numbers_str)
+    numbers.split(delimiter).map(&:to_i)
+  end
+
+  def split_delimiter_and_numbers(numbers_str)
+    delimiter = DEFAULT_DELIMITER
+    if numbers_str.start_with?(CUSTOM_DELIMITER_PREFIX)
+      delimiter = numbers_str.split(CUSTOM_DELIMITER_PREFIX)[1][0]
       numbers_str = numbers_str.split("\n")[1]
+    else
+      numbers_str = numbers_str.gsub("\n", delimiter)
     end
 
-    numbers_str.gsub("\n", delimiter).split(delimiter).map(&:to_i)
+    [delimiter, numbers_str]
   end
 end
