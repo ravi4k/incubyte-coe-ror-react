d40c0cd5baae336214e6969b815b13819a263514
Ravi K <ravi4kumawat@gmail.com>
2026-01-30 16:34:06 +0530

    Update App tests to verify rendering of Greeting component and title

--- javascript-react-intro/react-train-app/src/App.test.tsx
+++ javascript-react-intro/react-train-app/src/App.test.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the App with Greeting component', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const greetingElement = screen.getByText(/React Developer/i);
+  expect(greetingElement).toBeInTheDocument();
+
+  const titleElement = screen.getByText(/Your Learning Journey Begins Here/i);
+  expect(titleElement).toBeInTheDocument();
 });
